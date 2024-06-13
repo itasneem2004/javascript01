8ce8c42a6c7af7df0788ea98bbfdb47ce34a2b56
itasneem2004 <idrisitasneembsk@gmail.com>
2024-06-13 17:46:10 +0530

    js.tutorial

---
+++ index.js
@@ -0,0 +1,139 @@
+// console.log("HELLO TASNEEM123");
+// console.log("Hello Techno Students");
+
+// // variables
+// let message="The Password Is Incorrect";
+// console.log(message);
+// alert(message);
+
+// let hello="aquib";
+// console.log(hello);
+
+// var newdata="haris";
+// console.log(newdata)
+
+// let messagebox
+// messagebox="world";
+// messagebox="hello";
+// console.log(messagebox)
+
+// let admin="john";
+// let name="john";
+// alert(admin);
+
+// const
+// const birthday="23.04.1969";
+// birthday="24.09.2004";
+// console.log(birthday);
+
+// Data type in Javascript
+// 1.number
+// let n= 123;
+// let nf= 12.345;
+// console.log(n)
+// console.log(nf)
+
+// 2.bigint
+// let bigint= 1234567890n;
+// console.log(bigint);
+
+// 3.string
+// let str ="HELLO";
+// let str2 ='single quotes are ok too';
+// let phrase= `${str} can embed another ${str2}`;
+// console.log(phrase);
+// alert(`the result is ${1 + 2}`);
+
+// 4.boolean
+// let isgreater=4<8;
+// alert(isgreater)
+
+// 5.null
+// its just a speacial value which represents "nothing","empty", or "value unknown".
+// let age="null";
+// console.log(age);
+
+// 6.undefine
+// let data;
+// console.log(data);
+
+// 7.object for more complex data structures.
+// 8. symbols for unique identifiers.
+
+// Basic Opertators
+// addition
+// let apples =15;
+// let mangoes =12;
+// alert(apples + mangoes);
+
+// 1.arithmetic operator
+// a1=20;
+// a2=50;
+// a3= a1*a2;
+// console.log(a3);
+
+// 2.assignment operator
+// a1=25
+//  a1 *=10
+//  console.log(a1);
+
+// let n=2;
+// n +=15;
+// console.log(n);
+
+// let t=400;
+// t /=2;
+// console.log(t);
+
+// let x=1;
+// x =-x;
+// console.log(x);
+
+// let z=1,y=3;
+// alert(y+z);
+
+// division
+//  8/3
+// alert(8/3);
+
+// exponential 2
+// alert(2 ** 2);
+// alert(2 ** 3);
+// alert(4 ** 2);
+
+// '1' is String ...therefore value is 12
+// alert('1'+2);
+// alert('2'+1);
+
+// alert(2+2+'1');
+
+// 3.string operator
+// let s="my"+"string";
+// alert(s);
+
+// 4.increment and decement operator 
+// ++x pre-increment ...increment x by 1 then return x 
+// x++ post-increment ...return x than increment by 1
+// --x pre-decrement ...decrement x by 1 then return x
+// x-- post-decrement ...return x than decrement by 1
+
+// let p;
+// p= 20;
+// console.log(++p);
+// console.log(p);
+
+// let q;
+// q= 45;
+// console.log(q++);
+// console.log(q);
+
+// let r;
+// r= 30;
+// console.log(--r);
+// console.log(r);
+
+// let s;
+// s= 56;
+// console.log(s--);
+// console.log(s);
+
